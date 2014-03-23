146c2c019bf7dce3ef8391482d353d7fafff83bb
Bruce Merry <bmerry@gmail.com>
2014-03-23 17:32:07 +0200

    Add a few sort algorithms

--- scanbench.cpp
+++ scanbench.cpp
@@ -13,6 +13,41 @@
 typedef std::chrono::high_resolution_clock clock_type;
 namespace compute = boost::compute;
 
+class vex_algorithm
+{
+protected:
+    vex::Context ctx;
+
+    vex_algorithm() : ctx(vex::Filter::Type(CL_DEVICE_TYPE_GPU))
+    {
+        if (!ctx)
+            throw std::runtime_error("No device found for vex");
+    }
+
+public:
+    void finish() { ctx.finish(); }
+};
+
+class clogs_algorithm
+{
+protected:
+    cl::Context ctx;
+    cl::Device device;
+    cl::CommandQueue queue;
+
+    clogs_algorithm()
+        : ctx(cl::Context::getDefault()),
+        device(cl::Device::getDefault()),
+        queue(cl::CommandQueue::getDefault())
+    {
+    }
+
+public:
+    void finish() { queue.finish(); }
+};
+
+/************************************************************************/
+
 template<typename T>
 class compute_scan
 {
@@ -34,50 +69,34 @@ public:
 };
 
 template<typename T>
-class vexcl_scan
+class vex_scan : public vex_algorithm
 {
-private:
-    vex::Context ctx;
+protected:
     vex::vector<T> d_a, d_scan;
 
 public:
-    vexcl_scan(const std::vector<T> &h_a)
-        : ctx(vex::Filter::Type(CL_DEVICE_TYPE_CPU))
-    {
-        if (!ctx)
-            throw std::runtime_error("No device found for vex");
-        d_a = vex::vector<T>(ctx, h_a);
-        d_scan = vex::vector<T>(ctx, h_a.size());
-    }
+    vex_scan(const std::vector<T> &h_a)
+        : d_a(vex::vector<T>(ctx, h_a)),
+        d_scan(vex::vector<T>(ctx, h_a.size())) {}
 
     std::string name() const { return "vex::exclusive_scan"; }
     void run() { vex::exclusive_scan(d_a, d_scan); }
-    void finish() { ctx.finish(); }
 };
 
 template<typename T>
-class vexcl_clogs_scan
+class vex_clogs_scan : public vex_scan<T>
 {
-private:
-    vex::Context ctx;
-    vex::vector<T> d_a, d_scan;
-
 public:
-    vexcl_clogs_scan(const std::vector<T> &h_a)
-        : ctx(vex::Filter::Type(CL_DEVICE_TYPE_CPU)), d_a(ctx, h_a), d_scan(ctx, h_a.size())
-    {
-    }
+    vex_clogs_scan(const std::vector<T> &h_a) : vex_scan<T>(h_a) {}
 
     std::string name() const { return "vex::clogs::exclusive_scan"; }
-    void run() { vex::clogs::exclusive_scan(d_a, d_scan); }
-    void finish() { ctx.finish(); }
+    void run() { vex::clogs::exclusive_scan(this->d_a, this->d_scan); }
 };
 
 template<typename T>
-class clogs_scan
+class clogs_scan : public clogs_algorithm
 {
 private:
-    cl::CommandQueue queue;
     std::size_t elements;
     cl::Buffer d_a;
     cl::Buffer d_scan;
@@ -85,10 +104,8 @@ private:
 
 public:
     clogs_scan(const std::vector<T> &h_a)
-        : queue(cl::CommandQueue::getDefault()),
-        elements(h_a.size()),
-        scan(cl::Context::getDefault(), cl::Device::getDefault(),
-             vex::clogs::clogs_type<T>::type())
+        : elements(h_a.size()),
+        scan(ctx, device, vex::clogs::clogs_type<T>::type())
     {
         std::vector<T> &h_a_nc = const_cast<std::vector<T> &>(h_a);
         cl::Context ctx = cl::Context::getDefault();
@@ -99,7 +116,6 @@ public:
 
     std::string name() const { return "clogs::Scan"; }
     void run() { scan.enqueue(queue, d_a, d_scan, elements); }
-    void finish() { queue.finish(); }
 };
 
 template<typename T>
@@ -130,6 +146,52 @@ public:
     void finish() {};
 };
 
+/************************************************************************/
+
+template<typename T>
+class vex_sort : public vex_algorithm
+{
+private:
+    vex::vector<T> d_a;
+    vex::vector<T> d_target;
+
+public:
+    vex_sort(const std::vector<T> &h_a) : d_a(ctx, h_a), d_target(ctx, h_a.size()) {}
+
+    std::string name() const { return "vex::sort"; }
+    void run() { d_target = d_a; vex::sort(d_target); }
+};
+
+template<typename T>
+class clogs_sort : public clogs_algorithm
+{
+private:
+    std::size_t elements;
+    cl::Buffer d_a;
+    cl::Buffer d_target;
+    clogs::Radixsort sort;
+
+public:
+    clogs_sort(const std::vector<T> &h_a)
+        : elements(h_a.size()),
+        d_a(ctx, CL_MEM_READ_WRITE, elements * sizeof(T)),
+        d_target(ctx, CL_MEM_READ_WRITE, elements * sizeof(T)),
+        sort(ctx, device, vex::clogs::clogs_type<T>::type())
+    {
+        queue.enqueueWriteBuffer(d_a, CL_TRUE, 0, elements * sizeof(T), h_a.data());
+    }
+
+    std::string name() const { return "clogs::Radixsort"; }
+
+    void run()
+    {
+        queue.enqueueCopyBuffer(d_a, d_target, 0, 0, elements * sizeof(T));
+        sort.enqueue(queue, d_target, cl::Buffer(), elements);
+    }
+};
+
+/************************************************************************/
+
 template<typename T>
 static void time_algorithm(T &&alg, int iter)
 {
@@ -154,10 +216,16 @@ int main()
         h_a[i] = i;
 
     time_algorithm(compute_scan<cl_int>(h_a), iter);
-    time_algorithm(vexcl_scan<cl_int>(h_a), iter);
-    time_algorithm(vexcl_clogs_scan<cl_int>(h_a), iter);
+    time_algorithm(vex_scan<cl_int>(h_a), iter);
+    time_algorithm(vex_clogs_scan<cl_int>(h_a), iter);
     time_algorithm(clogs_scan<cl_int>(h_a), iter);
     time_algorithm(serial_scan<cl_int>(h_a), iter);
     time_algorithm(parallel_scan<cl_int>(h_a), iter);
+
+    std::vector<cl_uint> rnd(20000000);
+    for (std::size_t i = 0; i < rnd.size(); i++)
+        rnd[i] = (cl_uint) i * 0x9E3779B9;
+    time_algorithm(vex_sort<cl_uint>(rnd), iter);
+    time_algorithm(clogs_sort<cl_uint>(rnd), iter);
     return 0;
 }
