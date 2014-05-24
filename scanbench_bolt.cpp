9e067d3d6a47ed5df90d450efff0357036a6a860
Bruce Merry <bmerry@gmail.com>
2014-05-24 18:42:30 +0200

    Add bolt_sort and fix Bolt get functions

--- scanbench_bolt.cpp
+++ scanbench_bolt.cpp
@@ -1,4 +1,5 @@
 #include <bolt/cl/scan.h>
+#include <bolt/cl/sort.h>
 #include <bolt/cl/copy.h>
 #include <bolt/cl/device_vector.h>
 #include <stdexcept>
@@ -43,10 +44,43 @@ public:
 
     virtual std::vector<T> get() const override
     {
-        std::vector<T> ans(d_scan.size());
-        //bolt::cl::copy(control, d_scan.begin(), d_scan.end(), ans.begin());
-        return ans;
+        auto ptr = d_scan.data();
+        return std::vector<T>(ptr.get(), ptr.get() + d_scan.size());
     }
 };
 
 static register_scan_algorithm<bolt_scan> register_bolt_scan;
+
+/************************************************************************/
+
+template<typename T>
+class bolt_sort : public sort_algorithm<T>, public bolt_algorithm
+{
+protected:
+    bolt::cl::device_vector<T> d_a, d_target;
+
+public:
+    bolt_sort(const std::vector<T> &h_a)
+        : sort_algorithm<T>(h_a), d_a(h_a.begin(), h_a.end(), CL_MEM_READ_WRITE, control),
+        d_target(h_a.size(), T(), CL_MEM_READ_WRITE, false, control)
+    {
+    }
+
+    static std::string name() { return "bolt::cl::sort"; }
+    static std::string api() { return "bolt"; }
+    virtual void finish() override { control.getCommandQueue().finish(); }
+
+    virtual void run() override
+    {
+        bolt::cl::copy(control, d_a.begin(), d_a.end(), d_target.begin());
+        bolt::cl::sort(control, d_target.begin(), d_target.end());
+    }
+
+    virtual std::vector<T> get() const override
+    {
+        auto ptr = d_target.data();
+        return std::vector<T>(ptr.get(), ptr.get() + d_target.size());
+    }
+};
+
+static register_sort_algorithm<bolt_sort> register_bolt_sort;
