4b5c94f02e381b242b65871d85c079eb62eae5ba
Bruce Merry <bmerry@gmail.com>
2014-05-27 13:31:50 +0200

    Convert vex to key-value sorting

--- scanbench_vex.cpp
+++ scanbench_vex.cpp
@@ -8,6 +8,48 @@
 #include "scanbench_register.h"
 #include "clutils.h"
 
+template<typename T>
+struct vex_traits
+{
+    typedef vex::vector<T> vector;
+
+    static std::vector<T> get(const vector &v)
+    {
+        std::vector<T> ans(v.size());
+        vex::copy(v, ans);
+        return ans;
+    }
+
+    template<typename K>
+    static void sort(vex::vector<K> &keys, vector &values)
+    {
+        vex::sort_by_key(keys, values);
+    }
+};
+
+template<>
+struct vex_traits<void>
+{
+    struct vector
+    {
+        vector() {}
+        vector(const vex::Context &ctx, const void_vector &) {}
+        vector(const vex::Context &ctx, std::size_t size) {}
+        std::size_t size() const { return 0; }
+    };
+
+    static void_vector get(const vector &v)
+    {
+        return void_vector();
+    }
+
+    template<typename K>
+    static void sort(vex::vector<K> &keys, vector &values)
+    {
+        vex::sort(keys);
+    }
+};
+
 class vex_algorithm
 {
 protected:
@@ -73,16 +115,25 @@ static register_scan_algorithm<vex_clogs_scan> register_vex_clogs_scan;
 
 /************************************************************************/
 
-template<typename T>
-class vex_sort : public sort_algorithm<T>, public vex_algorithm
+template<typename K, typename V>
+class vex_sort : public sort_algorithm<K, V>, public vex_algorithm
 {
 protected:
-    vex::vector<T> d_a;
-    vex::vector<T> d_target;
+    typedef typename vector_of<K>::type key_vector;
+    typedef typename vector_of<V>::type value_vector;
+    typedef typename vex_traits<K>::vector d_key_vector;
+    typedef typename vex_traits<V>::vector d_value_vector;
+
+    d_key_vector d_keys, d_sorted_keys;
+    d_value_vector d_values, d_sorted_values;
 
 public:
-    vex_sort(device_type d, const std::vector<T> &h_a)
-        : sort_algorithm<T>(h_a), vex_algorithm(d), d_a(ctx, h_a), d_target(ctx, h_a.size())
+    vex_sort(device_type d, const key_vector &h_keys, const value_vector &h_values)
+        : sort_algorithm<K, V>(h_keys, h_values), vex_algorithm(d),
+        d_keys(ctx, h_keys),
+        d_sorted_keys(ctx, h_keys.size()),
+        d_values(ctx, h_values),
+        d_sorted_values(ctx, h_values.size())
     {
     }
 
@@ -92,15 +143,16 @@ public:
 
     virtual void run() override
     {
-        d_target = d_a;
-        vex::sort(d_target);
+        d_sorted_keys = d_keys;
+        d_sorted_values = d_values;
+        vex_traits<V>::template sort<K>(d_sorted_keys, d_sorted_values);
     }
 
-    virtual std::vector<T> get() const override
+    virtual std::pair<key_vector, value_vector> get() const override
     {
-        std::vector<T> ans(d_target.size());
-        vex::copy(d_target, ans);
-        return ans;
+        return std::make_pair(
+            vex_traits<K>::get(d_sorted_keys),
+            vex_traits<V>::get(d_sorted_values));
     }
 };
 
